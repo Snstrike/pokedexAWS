41f135ec89ffad2628977e2845452f55c7ed5176
Olmos <114839871+Snstrike@users.noreply.github.com>
2024-05-28 17:41:09 -0600

    Update get_pokemon_list.php

--- get_pokemon_list.php
+++ get_pokemon_list.php
@@ -12,14 +12,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar conexión
 if ($conn->connect_error) {
-    die(json_encode(["error" => "Error de conexión: " . $conn->connect_error]));
+    echo json_encode(["error" => "Error de conexión: " . $conn->connect_error]);
+    exit();
 }
 
 $query = 'SELECT id, name, no, image FROM pokemon ORDER BY no';
 $result = $conn->query($query);
 
 if ($result === false) {
-    die(json_encode(["error" => "Error en la consulta: " . $conn->error]));
+    echo json_encode(["error" => "Error en la consulta: " . $conn->error]);
+    $conn->close();
+    exit();
 }
 
 $pokemonList = [];
